d4e78c774c4967cdf321c4327212f5919e9e0786
Archinime <archinime12@gmail.com>
2026-03-03 13:16:04 -0500

    Add files via upload

--- inventory-data.js
+++ inventory-data.js
@@ -220,7 +220,8 @@ export const defaultInventoryConfig = {
                 name: "Póster 1 Estándar", 
                 file: "https://cdn.jsdelivr.net/gh/Archinime/ArchiPapu@main/poster_1.glb", 
                 price: 0, 
-                owned: true 
+                owned: true,
+                preview: "https://cdn.jsdelivr.net/gh/Archinime/ArchiPapu@main/poster_hero_academy.avif"
             } 
         } 
     },
