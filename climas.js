44f0a215c24adac3814217b052703348283c27e8
Archinime <archinime12@gmail.com>
2026-03-05 21:48:36 -0500

    Add files via upload

---
+++ climas.js
@@ -0,0 +1,11 @@
+// climas.js
+export const climaConfig = {
+    // Objeto de clima (día soleado) - se carga automáticamente al iniciar la escena
+    dia_soleado: {
+        file: "https://cdn.jsdelivr.net/gh/Archinime/ArchiPapu@main/dia_soleado.glb",
+        // Puedes ajustar la posición, rotación o escala si es necesario
+        position: [0, 5, -8],  // Ejemplo: colocado detrás de la habitación, un poco elevado
+        scale: 1.5,
+        rotation: [0, 0, 0]
+    }
+};
